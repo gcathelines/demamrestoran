d332836294636f299f8eaed6caedcfdb147fe35b
Catheline <gabriellyan.sanger@binus.ac.id>
2019-01-22 15:07:49 +0700

    added music

--- js/game.js
+++ js/game.js
@@ -6,9 +6,18 @@ const POSITION_LEFT = 1;
 const POSITION_RIGHT = 2;
 const POSITION_CENTER = 3;
 
+var good = new Audio();
+var wrong = new Audio();
+var bgm = new Audio();
+good.src = "url('./../music/v.wav"
+bgm.src = "url('./../music/bgm.mp3";
+wrong.src = "url('./../music/x.wav";
+
+let score = 0;
+let time = 60;
+let prevPosition = -1;
 let foods = new Array();
 let tID = null;
-
 let isAnimationDone = true;
 
 function generateFoods(){
@@ -25,6 +34,9 @@ function drawFood(){
 
 function createFood(){
     let position = Math.floor(Math.random() * 3)+1;
+    while(prevPosition == position)
+        position = Math.floor(Math.random() * 3)+1;
+    prevPosition = position;
     let image = Math.floor(Math.random() * 4)+1;
     let newFood = document.createElement("div"); 
     if(position == 1){
@@ -48,47 +60,58 @@ function keyEvent(e) {
     if (key == KEY_LEFT && isAnimationDone) {
         if(curr.getAttribute("position") == POSITION_LEFT){
             document.getElementById("person").style.backgroundImage = "url(image/person-eat-left.png)";
+            score+=10;
+            document.getElementById("score").innerText = "Score: " + score;
             animateScript(-11, 185.25);
             foods.splice(0, 1);
             curr.remove();
+            good.play();
             drawFood();
+            good.play();
         }
         else{
             document.getElementById("person").style.backgroundImage = "url(image/person-wrong-left.png)";
             animateScript(-11, 185.25);
+            wrong.play();
         }
         
     }else if (key == KEY_DOWN || key == KEY_UP && isAnimationDone) {
         if(curr.getAttribute("position") == POSITION_CENTER){
             document.getElementById("person").style.backgroundImage = "url(image/person-eat-right.png)";
+            score+=10;
+            document.getElementById("score").innerText = "Score: " + score;
             animateScript(-11, 185.25);
             foods.splice(0, 1);
             curr.remove();
+            good.play();
             drawFood();
         }
         else{
             document.getElementById("person").style.backgroundImage = "url(image/person-wrong-left.png)";
             animateScript(-11, 185.25);
+            wrong.play();
         }
 
     }else if(key == KEY_RIGHT && isAnimationDone){
         if(curr.getAttribute("position") == POSITION_RIGHT){
             document.getElementById("person").style.backgroundImage = "url(image/person-eat-right.png)";
+            score+=10;
+            document.getElementById("score").innerText = "Score: " + score;
             animateScript(-11, 185.25);
             foods.splice(0, 1);
             curr.remove();
+            good.play();
             drawFood();
         }
         else{
             document.getElementById("person").style.backgroundImage = "url(image/person-wrong-right.png)";
             animateScript(-11, 185.25);
+            wrong.play();
         }
     }
 }
 
 function toIdle(){
-    if(tID)
-        clearInterval(tID);
     document.getElementById("person").style.backgroundImage = "url(image/person-idle.png)";
     document.getElementById("person").style.backgroundPosition = `-11px 0px`;
     isAnimationDone = true;
@@ -107,12 +130,32 @@ function animateScript(position, addition) {
             clearInterval(tID);
             toIdle();
         }
-        
     }
     , interval ); 
 }
 
 window.onkeyup = keyEvent;
 window.onload = function(){
+    play();
+}
+
+function play(){
+    score = 0;
+    time = 60;
+    prevPosition = -1;
+    foods = new Array();
+    tID = null;
+    isAnimationDone = true;
+    bgm.play();
+    let timeTag = document.getElementById("time");
+    let timer = setInterval(function(){
+        if(time == 0){
+            alert("Total Score: "+score+"\nPress Ok to play again");
+            clearInterval(timer);
+            location.reload();
+        } 
+        timeTag.innerText = "Time: " + time + " sec";
+        time--;
+    },1000);
     generateFoods();
 }
